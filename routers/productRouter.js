deb702ca885d399331f7d173b0fa33a7121a9436
giovannimazzi <gioelor@libero.it>
2026-04-01 14:55:14 +0200

    add homepage and availability endpoints to product router

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
+router.get("/homepage", productController.homepage);
 router.get("/", productController.index);
 router.get("/:slug", productController.show);
+
+router.post("/availability", productController.availability);
 router.post("/", productController.store);
+
 router.put("/:slug", productController.update);
 router.patch("/:slug", productController.modify);
 router.delete("/:slug", productController.destroy);
