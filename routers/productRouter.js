a99f62813c5b20ab06f98c8c6d83928fcd6422f5
giovannimazzi <gioelor@libero.it>
2026-04-03 16:01:53 +0200

    modify homepage and availability routes

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
-router.get("/homepage", productController.homepage);
+router.get("/u/homepage", productController.homepage);
 router.get("/", productController.index);
 router.get("/:slug", productController.show);
 
-router.post("/availability", productController.availability);
+router.post("/u/availability", productController.availability);
 router.post("/", productController.store);
 
 router.put("/:slug", productController.update);
